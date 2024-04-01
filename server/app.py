471deae6f372d6bdd14670cd2ea74a24d90e26b9
Gabriella Richmond <gabriella.m.richmond@gmail.com>
2024-04-01 14:51:54 -0700

    flask sqlalchemy lab 1

--- server/app.py
+++ server/app.py
@@ -21,6 +21,27 @@ def index():
     return make_response(body, 200)
 
 # Add views here
+@app.route('/earthquakes/<int:id>')
+def earthquake_by_id(id):
+    earthquake = Earthquake.query.filter(Earthquake.id == id).first()
+    if earthquake:
+        body = earthquake.to_dict()
+        status = 200
+    else:
+        body = {'message': f'Earthquake {id} not found.'}
+        status = 404
+    return make_response(body, status)
+
+@app.route('/earthquakes/magnitude/<float:magnitude>')
+def magnitude(magnitude):
+    all = []
+    magnitude_req = Earthquake.query.filter(Earthquake.magnitude >= magnitude).all()
+    for earthquake in magnitude_req:
+        all.append(earthquake.to_dict())
+    body = {'count': len(all), 
+            'quakes': all
+            }
+    return make_response(body, 200)
 
 
 if __name__ == '__main__':
